6c794d01a290bc82890c654408d178e823f733ea
Oleg Levchenko <oleg.levchenko.dev@gmail.com>
2019-07-19 22:23:48 +0300

    Styled react select

--- src/components/Header/HeaderBottom.js
+++ src/components/Header/HeaderBottom.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 import MainMenu from 'components/Header/MainMenu';
 import Col from 'components/Header/HeaderCol';
 import Row from 'components/Header/HeaderRow';
 import Support from 'components/Support';
+import Select from 'components/Select';
 
 const NavStyled = styled.nav`
   height: 100%;
 `;
 
+const options = [
+  {value: 'chocolate', label: 'Chocolate'},
+  {value: 'strawberry', label: 'Strawberry'},
+  {value: 'vanilla', label: 'Vanilla'},
+];
+
 function HeaderBottom() {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
   return (
     <Row>
       <Col>
@@ -20,7 +28,12 @@ function HeaderBottom() {
         </NavStyled>
       </Col>
       <Col>
-        Right
+        <Select
+          classNamePrefix="react-select"
+          //styles={customStyles}
+          value={selectedOption}
+          onChange={setSelectedOption}
+          options={options}/>
       </Col>
     </Row>
   );
---
+++ src/components/Select.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactSelect from 'react-select';
+
+const SelectWrap = styled.nav`
+  color: #3c4252;
+  font-size: 14px;
+  font-weight: 400;
+  text-transform: uppercase;
+  .react-select__control {
+    cursor: pointer;
+    width: 200px;
+    border-radius: 0;
+    height: 43px;
+    border: 1px solid #e8ebf5 !important;
+    background-color: #fff;
+    box-shadow: none;
+  }
+  .react-select__value-container {
+    padding: 0 15px;
+  }
+  .react-select__indicator-separator {
+    display: none;
+  }
+  .react-select__menu{
+    top: 0;
+    left: 0;
+    margin: 0;
+    border-radius: 0;
+    box-shadow: 0 9px 34px rgba(79, 113, 154, 0.18);
+    border-bottom: 1px solid #fff;
+  }
+  .react-select__menu-list {
+    padding: 0;
+  }
+  .react-select__option {
+    cursor: pointer;
+    height: 43px;
+    display: flex;
+    padding: 0 18px;
+    align-items: center;
+    background-color: #fff !important;
+    &:hover{
+      background-color: #f7f9fc !important;
+    }
+  }      
+  .react-select__option--is-selected {
+    background: #f7f9fc !important;    
+    color: #3c4252;
+  }
+  .react-select__indicators {
+    position: relative;
+    z-index: 2;  
+  }
+  .react-select__indicator {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 7px 3.5px 0 3.5px;
+    border-color: #3c4252 transparent transparent transparent;
+    padding: 0;
+    margin: 0 19px 0 11px;
+    svg {
+      display: none;
+    }
+  }
+`;
+
+function Select({value, onChange, options}) {
+  return (
+    <SelectWrap>
+      <ReactSelect
+        //menuIsOpen
+        classNamePrefix="react-select"
+        isSearchable={false}
+        value={value}
+        onChange={onChange}
+        options={options}/>
+    </SelectWrap>
+  );
+}
+
+export default Select;
